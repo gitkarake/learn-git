3c630570f3be1b0ae77521902da5d374f1ef2028
gitkarake <g.karake@proton.me>
2023-01-04 22:40:47 +0300

    newBranchHasBeenCommited

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT
 const num = 10;
 const num2= 20;
 const num3= 30;
+const myNewBranch = true;
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
 
