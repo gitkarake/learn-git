f9233c4990991e8e11a2116ef20aa412779458a7
gitkarake <g.karake@proton.me>
2023-01-04 22:00:30 +0300

    fromSecondBranch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 const num = 10;
+const num2= 20;
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
 
