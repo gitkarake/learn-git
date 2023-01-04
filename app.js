ebc64bc1ba31612d70f0c34029c643355b4f6214
gitkarake <g.karake@proton.me>
2023-01-04 21:54:59 +0300

    mySecond

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT
+const num = 10;
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
 
