0dd5fdd09467813002f0fd4fd501b46e06b8f6bc
gitkarake <g.karake@proton.me>
2023-01-04 21:53:47 +0300

    myInitialise

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
+
